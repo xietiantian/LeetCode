f7082b3a1a35d86d453190ac9ec0b73078e3fa15
xietianrui <xtr@bupt.edu.cn>
2016-09-30 14:09:27 +0800

    20160930 / 34 problems

--- src/com/leetcode/Problem070.java
+++ src/com/leetcode/Problem070.java
@@ -8,7 +8,34 @@ package com.leetcode;
  * can you climb to the top?
  */
 public class Problem070 {
-    public int climbStairs(int n) {
-        
+    //把算过的值存起来，否则计算次数太多会超时
+    public static final int[] F = new int[45];//climbStairs(46)会超出Integer.MAX_VALUE
+    public static int climbStairs(int n) {
+        if(n<=0){
+            return 1;
+        }
+        if(n==1){
+            if(F[n]==0){
+                F[n]=1;
+            }
+            return 1;
+        }else if(n==2){
+            if(F[n]==0){
+                F[n]=2;
+            }
+            return 2;
+        }else{
+            if(F[n-1]==0){
+                F[n-1]=climbStairs(n-1);
+            }
+            if(F[n-2]==0){
+                F[n-2]=climbStairs(n-2);
+            }
+            return F[n-1]+F[n-2];
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(45));
     }
 }
